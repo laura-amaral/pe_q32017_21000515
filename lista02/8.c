aee45d5a5c15b6cdb48684259d1da0c577fc5ebb
laura-amaral <32200204+laura-amaral@users.noreply.github.com>
2017-10-20 20:20:46 -0200

    Ex 8-9

---
+++ lista02/8.c
@@ -0,0 +1,18 @@
+/* faz cacula os salarios:
+ * 1. cargo
+ *      1. Diretor - 10.000
+ *      2. Gerente - 8.000
+ *      3. Engenheiro - 5.000
+ *      4. Tecnico - 3.000
+ *      5. Operador - 2.000
+ * 2. binario
+ *      1. decimal
+ *      2. binario
+ *
+ * Autor: Laura Amaral
+ * Data: 20/10/2017
+ */
+
+#include <stdio.h>
+#include <math.h>
+
---
+++ lista02/9.c
@@ -0,0 +1,27 @@
+/* calcula a potencia
+ *
+ * Autor: Laura Amaral
+ * Data: 20/10/2017
+ */
+ #include <stdio.h>
+ #include <math.h>
+
+int potencia(int num1, int num2){
+
+    if (!num2){
+        return 1;
+    }
+
+    return num1*pot(num1,num2-1);
+
+}
+
+
+ int main()    {
+    int num1,num2;
+
+    printf ("Entrada: ");
+    scanf("%d %d",&num1,&num2);
+    printf ("Saida: %d\n", potencia(num1,num2));
+}
+
